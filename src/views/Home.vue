52aa685802b2b53716d0c2217101082c3488ddf6
Zain Albastaki <zain.albastaki@gmail.com>
2020-06-09 14:40:30 +0300

    Change soldiers being previewed every 5 seconds

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,15 @@
 <template>
     <div id="home-page">
         <soldier-preview
-            v-for="(soldier, index) in data.soldiers"
+            v-for="(soldier, index) in soldiers"
             :key="index"
             :name="soldier.name"
             :photo="soldier.photo"
         />
+
+        <div class="search-container">
+            Search
+        </div>
     </div>
 </template>
 
@@ -22,6 +26,43 @@
         components: {
             SoldierPreview,
         },
+
+        data() {
+            return {
+                soldiers: [],
+            };
+        },
+
+        mounted() {
+            this.selectSoldiers();
+            setInterval(this.selectSoldiers, 5000);
+        },
+
+        methods: {
+            selectSoldiers() {
+                this.soldiers = this.getRandom(this.data.soldiers, 10);
+            },
+
+            getRandom(arr, n) {
+                let result = new Array(n);
+                let len = arr.length;
+                let taken = new Array(len);
+
+                if (n > len) {
+                    throw new RangeError(
+                        'getRandom: more elements taken than available'
+                    );
+                }
+
+                while (n--) {
+                    var x = Math.floor(Math.random() * len);
+                    result[n] = arr[x in taken ? taken[x] : x];
+                    taken[x] = --len in taken ? taken[len] : len;
+                }
+
+                return result;
+            },
+        },
     };
 </script>
 
@@ -32,5 +73,15 @@
         display: grid;
         grid-template-columns: repeat(4, 1fr);
         grid-template-rows: repeat(3, 1fr);
+
+        .search-container {
+            grid-row: 2;
+            grid-column-start: 2;
+            grid-column-end: 4;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+        }
     }
 </style>
